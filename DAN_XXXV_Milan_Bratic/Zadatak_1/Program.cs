93b08b18ba8b1b672f561d67e882c441ec26de98
MilanBratic0512 <milan.bratic2@gmail.com>
2020-06-19 12:09:50 +0200

    making thread generator method

--- DAN_XXXV_Milan_Bratic/Zadatak_1/Program.cs
+++ DAN_XXXV_Milan_Bratic/Zadatak_1/Program.cs
@@ -2,14 +2,39 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Zadatak_1
 {
     class Program
     {
+        static void Thread_Generator(uint players, uint number)
+        {
+            List<Thread> threadList = new List<Thread>();
+            for (int i = 0; i < players; i++)
+            {
+                Thread t = new Thread(() => GuessingNumber(number));
+                threadList.Add(t);
+            }
+            
+        }
+       
+        static void GuessingNumber(uint number)
+        {
+
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Input the number of players: ");
+            uint players = uint.Parse(Console.ReadLine());
+            Console.WriteLine("Input the number to be guessed: ");
+            uint number = uint.Parse(Console.ReadLine());
+
+            Thread thread = new Thread(() => Thread_Generator(players, number));
+            thread.Start();
+            Thread[] threads = new Thread[players];
+
             Console.ReadKey();
             
         }
