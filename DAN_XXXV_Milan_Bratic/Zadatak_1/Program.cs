abcb2c1abc126eb3fe79b5f87726278d02269fd0
MilanBratic0512 <milan.bratic2@gmail.com>
2020-06-19 14:29:33 +0200

    add validations

--- DAN_XXXV_Milan_Bratic/Zadatak_1/Program.cs
+++ DAN_XXXV_Milan_Bratic/Zadatak_1/Program.cs
@@ -13,40 +13,65 @@ namespace Zadatak_1
         static int guessNumber;
         private static object locker = new object();
 
+        /// <summary>
+        /// method for generatin threads
+        /// </summary>
+        /// <param name="players">pass number of players</param>
+        /// <param name="number">pass guess number</param>
+        /// <returns>list of threads</returns>
         static List<Thread> Thread_Generator(uint players, uint number)
         {
             List<Thread> threadList = new List<Thread>();
-            for (int i = 0; i < players; i++)
+            try
             {
-                Thread t = new Thread(() => GuessingNumber(number));
-                t.Name = "Player_" + (i + 1);
-                threadList.Add(t);
+                for (int i = 0; i < players; i++)
+                {
+                    //create threads
+                    Thread t = new Thread(() => GuessingNumber(number));
+                    //generate thread name
+                    t.Name = "Player_" + (i + 1);
+                    //adding to the list
+                    threadList.Add(t);
+                }
             }
+            catch (System.OutOfMemoryException)
+            {
+                Console.WriteLine("Error");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Error");
+            }
+
             return threadList;
 
         }
-
+        /// <summary>
+        /// method for guessing number
+        /// </summary>
         static void GuessingNumber(uint number)
         {
-            
+            //loop rotate until guess number
             while (number != guessNumber)
             {
 
                 lock (locker)
                 {
                     Thread.Sleep(100);
+                    //generate random number
                     guessNumber = rnd.Next(1, 101);
-                    
+
                     Console.WriteLine(Thread.CurrentThread.Name + " tried with the number " + guessNumber);
+                    //if contidion for parity
                     if (number % 2 == 0 && guessNumber % 2 == 0 || number % 2 != 0 && guessNumber % 2 != 0)
                     {
                         Console.WriteLine(Thread.CurrentThread.Name + " hit the parity of the number");
                     }
+                    //if guess number
                     if (guessNumber == number)
                     {
                         Console.WriteLine(Thread.CurrentThread.Name + " win. He hit required number.");
-                        
-                        Console.WriteLine("If you want to exit, press enter");
+                        Console.WriteLine("Press enter to exit");
                         Console.ReadLine();
                         Environment.Exit(0);
                     }
@@ -54,24 +79,54 @@ namespace Zadatak_1
 
             }
         }
-
+        /// <summary>
+        /// Method for start game, ask user how much players play and what is guess number
+        /// </summary>
+        /// <returns>array of two element that represent players and guess number</returns>
+        static uint[] StartGame()
+        {
+            uint[] array = new uint[2];
+            Console.WriteLine("Input the number of players: ");
+            string input = null;
+            uint players = 0;
+            //validations
+            do
+            {
+                input = Console.ReadLine();
+                if (!uint.TryParse(input, out players))
+                {
+                    Console.WriteLine("Wrong entry! Try again.");
+                }
+            } while (!uint.TryParse(input, out players));
+            Console.WriteLine("Input the number to be guessed: ");
+            string input2 = null;
+            uint number = 0;
+            //validations
+            do
+            {
+                input2 = Console.ReadLine();
+                if (!(uint.TryParse(input2, out number) && number > 0 && number < 101))
+                {
+                    Console.WriteLine("Wrong entry! Number must be between 1 and 100. Try again.");
+                }
+            } while (!(uint.TryParse(input2, out number) && number > 0 && number < 101));
+            array[0] = players;
+            array[1] = number;
+            return array;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Input the number of players: ");
-            uint players = uint.Parse(Console.ReadLine());
-            Console.WriteLine("Input the number to be guessed: ");
-            uint number = uint.Parse(Console.ReadLine());
+            uint[] array = StartGame();
 
-            Thread thread = new Thread(() => Thread_Generator(players, number));
+            Thread thread = new Thread(() => Thread_Generator(array[0], array[1]));
             thread.Start();
-            List<Thread> list = Thread_Generator(players, number);
-
+            Console.WriteLine("Number of players {0}, guess number is {1}", array[0], array[1]);
+            List<Thread> list = Thread_Generator(array[0], array[1]);
+            //start all threads
             foreach (Thread t in list)
             {
                 t.Start();
             }
-
-
             Console.ReadKey();
 
         }
